cd866552ade52fdab9708df31a036d3bb9e13cfb
thereselau1902 <thereselau1902@gmail.com>
2022-10-19 23:54:45 +0800

    updated orders controller to use cookei data instead of hardcoded values

--- controllers/orders.controller.mjs
+++ controllers/orders.controller.mjs
@@ -5,9 +5,6 @@ class OrdersContoller {
   }
   postNewOrder = async (request, response) => {
     // TODO: on add to cart, update local storage cookies of full_order
-    // full_order = array of object { model_id, model_name, model_params, qty, price_per_unit }
-    // retrieve these values to complete order_details
-    // order_details = firstname, lastname, address, phone, full_order
 
     // Verify if user is logged in before postNewOrder
     const loggedIn = this.userController.verify(request, response);
@@ -22,28 +19,8 @@ class OrdersContoller {
 
     const body = request.body;
     const cookies = request.cookies;
-    console.log("looking for cookie values, please print");
-    console.log("temp_cart", cookies["temp_cart"]);
-    console.log("saved-models", cookies["saved-models"]);
-    console.log("user", cookies["user"]);
 
-    // TODO: remove hardcoded values when we can get them from cookies later
-    const fullOrder = [
-      {
-        modelId: 11,
-        modelName: "apple pixelated",
-        modelParams: { color: "red", material: "PLA" },
-        qty: 1,
-        ppu: 15,
-      },
-      {
-        modelId: 12,
-        modelName: "boat simple",
-        modelParams: { color: "blue", material: "PLA" },
-        qty: 1,
-        ppu: 30,
-      },
-    ];
+    const fullOrder = JSON.parse(cookies["temp_cart"]);
 
     const orderDetails = {
       firstname: body.firstname,
@@ -57,7 +34,7 @@ class OrdersContoller {
     // user must be logged in for successful POST req
 
     const amount = fullOrder.reduce((accumulator, order) => {
-      return accumulator + order.ppu * order.qty;
+      return accumulator + order.ppu * order.quantity;
     }, 0);
 
     const order = await this.db.Order.create({
